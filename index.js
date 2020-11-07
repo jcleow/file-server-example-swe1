b87a9c586fdeb99a3d9933d99b4cb691297e3a78
jcleow <jc.leow94@gmail.com>
2020-11-07 11:49:36 +0800

    Added mime types

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import { createServer } from 'http';
 import { readFile } from 'fs';
+import path from 'path'
 
 const PORT = process.argv[2];
 
@@ -8,11 +9,32 @@ const whenIncomingRequest = (request, response) => {
 
   var filePath = '.' + request.url;
 
+  const extName = String(path.extname(filePath)).toLowerCase();
+  const mimeTypes = {
+    '.html': 'text/html',
+    '.js': 'text/javascript',
+    '.css': 'text/css',
+    '.json': 'application/json',
+    '.png': 'image/png',
+    '.jpg': 'image/jpg',
+    '.gif': 'image/gif',
+    '.svg': 'image/svg+xml',
+    '.wav': 'audio/wav',
+    '.mp4': 'video/mp4',
+    '.woff': 'application/font-woff',
+    '.ttf': 'application/font-ttf',
+    '.eot': 'application/vnd.ms-fontobject',
+    '.otf': 'application/font-otf',
+    '.wasm': 'application/wasm',
+    '.mp3': 'audio/mpeg',
+  };
+    const contentType = mimeTypes[extName];
+
   readFile(filePath, (error, content) => {
     if (error){
       console.log(error,'error');
     }else{
-    response.writeHead(200, { 'Content-Type': 'text/html' });
+    response.writeHead(200, { 'Content-Type': contentType });
     response.end(content, 'utf-8');
     }
 })
